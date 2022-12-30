534f5c6f6100854a3fce94eddcdc6de38f831583
Jacob Nardone <jacob.nardone@intellimindcorp.net>
2022-12-30 10:47:02 -0800

    added python-mode and yaml-mode packages

--- init.el
+++ init.el
@@ -135,6 +135,7 @@
 	  ("NOTE"       . "#db621d")
 	  ("FIXME"      . "#eb4034")
 	  ("NOCHECKIN"  . "#e8ce27")
+	  ("CHECKIN"    . "#e8ce27")
 	 )
 )
 
@@ -206,7 +207,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Liberation Mono" :foundry "outline" :slant normal :weight normal :height 120 :width normal)))))
+ '(default ((t (:family "Liberation Mono" :foundry "outline" :slant normal :weight normal :height 110 :width normal)))))
 
 
 
